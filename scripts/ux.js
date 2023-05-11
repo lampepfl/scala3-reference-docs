d88b4f5e9fab2cc9f139b4ae5ed948bd2d0c7cf8
gh-actions <actions@github.com>
2023-05-11 07:43:25 +0000

    UPDATE 2023-05-11

--- scripts/ux.js
+++ scripts/ux.js
@@ -309,10 +309,6 @@ document
       var selected = document.getElementById(location.hash.substring(1));
       if (selected) {
         selected.classList.toggle("expand");
-        selected.classList.toggle("expanded");
-        const btn = selected.querySelector(".icon-button");
-        btn.classList.toggle("expand");
-        btn.classList.toggle("expanded");
       }
     }
   }
